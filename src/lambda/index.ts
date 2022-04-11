f8436d49ef9a9341b9966281527a52f967b36219
Will Dady <willdady@gmail.com>
2022-04-11 15:18:06 +1000

    feat: collect polcies for Lambda Aliases and Versions

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -3,6 +3,8 @@ import {
   GetPolicyCommand,
   ResourceNotFoundException,
   paginateListFunctions,
+  paginateListAliases,
+  paginateListVersionsByFunction,
 } from '@aws-sdk/client-lambda';
 import { BasePolicyCollector, ServicePoliciesResult } from '../core';
 
@@ -23,12 +25,28 @@ export class LambdaPolicyCollector extends BasePolicyCollector {
     return fns;
   }
 
-  private async listAliases() {
-    // TODO
+  private async listAliases(functionName: string) {
+    const paginator = paginateListAliases(
+      { client: this.client },
+      { FunctionName: functionName },
+    );
+    const aliases = [];
+    for await (const page of paginator) {
+      aliases.push(...(page.Aliases || []));
+    }
+    return aliases;
   }
 
-  private async listVersions() {
-    // TODO
+  private async listVersions(functionName: string) {
+    const paginator = paginateListVersionsByFunction(
+      { client: this.client },
+      { FunctionName: functionName },
+    );
+    const versions = [];
+    for await (const page of paginator) {
+      versions.push(...(page.Versions || []));
+    }
+    return versions;
   }
 
   private async getPolicy(functionName: string, qualifier?: string) {
@@ -47,6 +65,42 @@ export class LambdaPolicyCollector extends BasePolicyCollector {
     };
     const functions = await this.listFunctions();
     for (const f of functions) {
+      const aliases = await this.listAliases(f.FunctionName!);
+      for (const a of aliases) {
+        try {
+          const response = await this.getPolicy(f.FunctionName!, a.Name!);
+          if (!response.Policy) continue;
+          result.resources.push({
+            type: 'AWS::Lambda::Alias',
+            id: [f.FunctionArn, ':', a.Name].join(''),
+            policy: response.Policy,
+          });
+        } catch (err) {
+          if (err instanceof ResourceNotFoundException) {
+            continue;
+          }
+          throw err;
+        }
+      }
+
+      const versions = await this.listVersions(f.FunctionName!);
+      for (const v of versions) {
+        try {
+          const response = await this.getPolicy(f.FunctionName!, v.Version!);
+          if (!response.Policy) continue;
+          result.resources.push({
+            type: 'AWS::Lambda::Version',
+            id: [f.FunctionArn, ':', v.Version].join(''),
+            policy: response.Policy,
+          });
+        } catch (err) {
+          if (err instanceof ResourceNotFoundException) {
+            continue;
+          }
+          throw err;
+        }
+      }
+
       try {
         const response = await this.getPolicy(f.FunctionName!);
         if (!response.Policy) continue;
